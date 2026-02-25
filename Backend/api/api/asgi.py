56c9333dc039861226652c3c01ca8ab35a752a18
navap <navaparajenil349@gmail.com>
2026-02-26 00:43:58 +0530

    fixed ASGI import order

--- Backend/api/api/asgi.py
+++ Backend/api/api/asgi.py
@@ -1,12 +1,14 @@
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from core.ws_auth import JWTAuthMiddlewareStack
-import core.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
+from django.core.asgi import get_asgi_application
+
 django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from core.ws_auth import JWTAuthMiddlewareStack
+import core.routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
